22c159879d2eee77711bdfcf7893aef310cc4120
CrBridge <91331576+CrBridge@users.noreply.github.com>
2024-08-15 11:25:49 +0100

    Basic procgen implementation

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::rect::Rect;
-//use std::time::Duration;
+use std::time::Duration;
+use noise::{NoiseFn, Perlin};
 
 pub fn main() {
     let sdl_context = sdl2::init().unwrap();
@@ -13,6 +14,7 @@ pub fn main() {
 
     let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
         .position_centered()
+        //.resizable()
         .build()
         .unwrap();
 
@@ -21,12 +23,30 @@ pub fn main() {
     canvas.set_draw_color(Color::RGB(0, 255, 255));
     canvas.clear();
     canvas.present();
+
+    //trying random gen
+    let perlin = Perlin::new(1);
+    let threshold = 0.5;
+    let scale = 0.2;
+    let map_size = 50;
+    let mut map: Vec<Vec<i32>> = Vec::new();
+    for y in 0..map_size {
+        let mut map_row: Vec<i32> = Vec::new();
+        for x in 0..map_size {
+            // Sample the noise function at different points with increased scale
+            let noise_value = perlin.get([x as f64 * scale, y as f64 * scale]);
+            // Normalize the noise value to be between 0.0 and 1.0
+            let normalized_value = (noise_value + 1.0) / 2.0;
+            // Threshold the normalized noise value to get binary output
+            let grid_value = if normalized_value > threshold { 1 } else { 0 };
+            map_row.push(grid_value);
+        }
+        map.push(map_row);
+    }
+    //end of rand gen
+
     let mut event_pump = sdl_context.event_pump().unwrap();
     'running: loop {
-        //i = (i + 1) % 255;
-        //canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-        canvas.set_draw_color(Color::RGB(255, 255, 255));
-        canvas.clear();
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit {..} |
@@ -39,14 +59,23 @@ pub fn main() {
         // The rest of the game loop goes here...
         let texture_loader = canvas.texture_creator();
         let tile = texture_loader.load_texture("assets/sprite_tile.png").unwrap();
-        //tile is 16 x 8 pixels
-        //canvas.set_draw_color(Color::RGB(0, 0, 0));
-        canvas.draw_rect(Rect::new(20, 20, 10, 10)).unwrap();
-        canvas.copy(&tile, None, Rect::new(100, 100, 200, 100)).unwrap();
-        canvas.copy(&tile, None, Rect::new(200, 150, 200, 100)).unwrap();
-        canvas.copy(&tile, None, Rect::new(300, 200, 200, 100)).unwrap();
+        canvas.set_draw_color(Color::RGB(0, 0, 0));
+        let tile_scale = 50;
 
+        // When I want more tiles, I should have all of them in one tilemap png,
+        // for individual tiles I can just pass the co-ords to src
+        for x in 0..map_size {
+            for y in 0..map_size{
+                if map[x][y] == 1 {
+                    canvas.copy(&tile, None, Rect::new(
+                        (x as i32 * tile_scale) - (y as i32 * tile_scale), 
+                        (x as i32 * tile_scale / 2) + (y as i32 * tile_scale / 2), 
+                        tile_scale as u32 * 2, tile_scale as u32)).unwrap();
+                    //canvas.draw_line((0, 0), (x * 100, y * 100)).unwrap();
+                }
+            }
+        }
         canvas.present();
-        //::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
 }
